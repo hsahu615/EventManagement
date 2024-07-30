1888ee7d6bc2ee3bf57bb11322b1b2330aa8c94f
Himanshu Sahu <hsahu615@gmail.com>
2024-07-30 20:04:38 +0530

    Implementing paypal payment integration

--- src/main/java/com/eventmanagement/server/config/SecurityConfig.java
+++ src/main/java/com/eventmanagement/server/config/SecurityConfig.java
@@ -33,6 +33,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/paypal/**").permitAll()
                         .requestMatchers("/auth/**").permitAll()
                         .requestMatchers("/user/all").hasAuthority("USER")
                         .anyRequest().hasAnyAuthority("ADMIN", "USER"))
---
+++ src/main/java/com/eventmanagement/server/controller/PaypalController.java
@@ -0,0 +1,83 @@
+package com.eventmanagement.server.controller;
+
+import com.eventmanagement.server.service.PaypalService;
+import com.paypal.api.payments.Links;
+import com.paypal.api.payments.Payment;
+import com.paypal.base.rest.PayPalRESTException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
+@RequestMapping("/paypal")
+@RequiredArgsConstructor
+@Slf4j
+public class PaypalController {
+
+    private final PaypalService paypalService;
+
+    @PostMapping("/payment/create")
+    public ResponseEntity<?> createPayment(
+            @RequestParam("method") String method,
+            @RequestParam("amount") String amount,
+            @RequestParam("currency") String currency,
+            @RequestParam("description") String description
+    ) {
+        try {
+            String cancelUrl = "https://www.google.com/";
+            String successUrl = "http://localhost:3000/payment/success";
+            Payment payment = paypalService.createPayment(
+                    Double.valueOf(amount),
+                    currency,
+                    method,
+                    "sale",
+                    description,
+                    cancelUrl,
+                    successUrl
+            );
+
+            for (Links links: payment.getLinks()) {
+                if (links.getRel().equals("approval_url")) {
+                    return new ResponseEntity<>(links.getHref(), HttpStatus.OK);
+                }
+            }
+        } catch (PayPalRESTException e) {
+            log.error("Error occurred:: ", e);
+        }
+        return new ResponseEntity<>("error", HttpStatus.OK);
+    }
+
+    @GetMapping("/payment/success")
+    public String paymentSuccess(
+            @RequestParam("paymentId") String paymentId,
+            @RequestParam("PayerID") String payerId
+    ) {
+        try {
+            Payment payment = paypalService.executePayment(paymentId, payerId);
+            if (payment.getState().equals("approved")) {
+                return "paymentSuccess";
+            }
+        } catch (PayPalRESTException e) {
+            log.error("Error occurred:: ", e);
+        }
+        return "paymentSuccess";
+    }
+
+    @GetMapping("/payment/cancel")
+    public String paymentCancel() {
+        return "paymentCancel";
+    }
+
+    @GetMapping("/payment/error")
+    public String paymentError() {
+        return "paymentError";
+    }
+}
